4d4732574c8329a9c656b0ca6b82c8c2a921c755
Diego Prystupczuk <d.r.prystupczuk@gmail.com>
2021-04-06 15:57:28 -0300

    Final commit

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { DeseosService } from 'src/app/services/deseos.service';
 import { Router } from '@angular/router';
 import { Lista } from 'src/app/models/lista.model';
+import { AlertController, IonList, IonListHeader } from '@ionic/angular';
 
 @Component({
   selector: 'app-listas',
@@ -10,16 +11,57 @@ import { Lista } from 'src/app/models/lista.model';
 })
 export class ListasComponent implements OnInit {
 
-
+@ViewChild(IonList) lista: IonList;
 @Input() tab = 1;
 
   constructor(public deseosService: DeseosService,
-    private router: Router) { }
+              private router: Router,
+              public alertController: AlertController) { }
 
   ngOnInit() {}
 
   abrirLista(lista: Lista) {
     this.router.navigateByUrl(`/tabs/tab${this.tab}/agregar/${lista.id}`);
+  }
+
+  async editarLista(lista: Lista){
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Editar lista',
+      // subHeader: 'Subtitle',
+      // message: 'This is an alert message.',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: 'Nombre de la lista',
+          value: lista.titulo
+        }
+      ],
+      buttons: [
+        {
+          text : 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            this.lista.closeSlidingItems();
+          }
+        },
+        {
+          text: 'Editar',
+          handler: (data) => {
+            console.log(data);
+            if ( !data.titulo.length ) {
+              return;
+            }
+            lista.titulo = data.titulo;
+            this.deseosService.guardarStorage();
+            this.lista.closeSlidingItems();
+          }
+        }
+      ]
+    });
+    alert.present();
+
   }
 
   borrarLista(lista: Lista){
